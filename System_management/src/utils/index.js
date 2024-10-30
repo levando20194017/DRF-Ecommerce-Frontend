5adfacad6313b5baa2a2dcbf32c8ea0c87e664b2
DO Le Van VTI.D9 <do.levan@vti.com.vn>
2024-10-30 19:31:13 +0700

    feat list promotion UI

--- System_management/src/utils/index.js
+++ System_management/src/utils/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 export const formatTime = (inputTime) => {
     const date = new Date(inputTime);
@@ -11,7 +11,7 @@ export const formatTime = (inputTime) => {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day}`;
 };
 
 export const toastSuccess = (title, path) => {
