3ee32b4189ad418cc15dd9aca7cec4200b6ba1fe
levando20194017 <levando20194017@gmail.com>
2024-10-30 08:39:38 +0700

    feat promotion UI

---
+++ System_management/src/components/product/promotion/CreatePromotion.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { Col, Row, Card, Form, Button } from "@themesberg/react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import { DatePicker, Space, theme } from 'antd';
+
+export const CreatePromotion = (props) => {
+    const [error, setError] = useState("")
+    const handleInput = (e) => {
+
+    }
+
+    const handleSubmit = () => {
+
+    }
+
+    const { token } = theme.useToken();
+    const style = {
+        border: `1px solid ${token.colorPrimary}`,
+        borderRadius: '50%',
+    };
+
+    const cellRender = (current, info) => {
+        if (info.type !== 'date') {
+            return info.originNode;
+        }
+        if (typeof current === 'number' || typeof current === 'string') {
+            return <div className="ant-picker-cell-inner">{current}</div>;
+        }
+        return (
+            <div className="ant-picker-cell-inner" style={current.date() === 1 ? style : {}}>
+                {current.date()}
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <ToastContainer />
+            <Col xs={12} xl={9}>
+                <Card border="light" className="bg-white shadow-sm mb-4">
+                    <Card.Body>
+                        <Form>
+                            <Row>
+                                <Col md={12} className="mb-3">
+                                    <Form.Group>
+                                        <Form.Label>Name<span className="text-danger"
+                                            style={{ paddingLeft: "1px" }}>*</span></Form.Label>
+                                        <Form.Control
+                                            required
+                                            type="text"
+                                            maxLength={100}
+                                            name="name"
+                                            placeholder="Enter promotion Name"
+                                            onChange={(e) => handleInput(e)}
+                                        />
+                                        {
+                                            error ? (<div className="text-danger">{error}</div>) : <></>
+                                        }
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={4} className="mb-3">
+                                    <Form.Group id="SKU">
+                                        <Form.Label>
+                                            Code <span className="text-danger">*</span>
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={8} className="mb-3">
+                                    <Form.Group id="SKU">
+                                        <Form.Label>
+                                            From Date <span className="text-danger">*</span>
+                                        </Form.Label>
+                                        <div>
+                                            <Space size={20} direction="vertical">
+                                                <DatePicker.RangePicker cellRender={cellRender} />
+                                            </Space>
+                                        </div>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={4} className="mb-3">
+                                    <Form.Group id="SKU">
+                                        <Form.Label>
+                                            Rate <span className="text-danger">*</span>
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={4} className="mb-3">
+                                    <Form.Group id="SKU">
+                                        <Form.Label>
+                                            Special Price
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={4} className="mb-3">
+                                    <Form.Group id="SKU">
+                                        <Form.Label>
+                                            Member Price
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Form.Label>Description</Form.Label>
+                            <Row className="px-2">
+                                <textarea rows={3} className="form-control" />
+                            </Row>
+                            <div className="mt-3">
+                                <Button variant="primary" onClick={() => handleSubmit()}>
+                                    Create Promotion
+                                </Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </>
+    );
+};
---
+++ System_management/src/pages/product/promotion/UpdatePromotion.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Breadcrumb } from "@themesberg/react-bootstrap";
+import { Row } from "@themesberg/react-bootstrap";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useHistory, useParams } from "react-router-dom";
+import { Routes } from "../../../routes";
+import { UpdatePromotion } from "../../../components/product/promotion/UpdatePromotion";
+
+export default () => {
+
+    const { id } = useParams();
+    const [promotion, setPromotion] = useState({})
+    const history = useHistory()
+
+    const handleUpdatePromotion = async (params, id) => {
+
+    }
+
+    return (
+        <>
+            <ToastContainer />
+            <div className="d-xl-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+                <div className="d-block mb-4 mb-xl-0">
+                    <Breadcrumb
+                        className="d-none d-md-inline-block"
+                        listProps={{
+                            className: "breadcrumb-dark breadcrumb-transparent",
+                        }}
+                    >
+                        <Breadcrumb.Item>
+                            <FontAwesomeIcon icon={faHome} />
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item onClick={() => history.push(Routes.Promotion.path)}>Promotion</Breadcrumb.Item>
+                        <Breadcrumb.Item active>{id ? 'Update Promotion' : 'Create Promotion'}</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <h4>Update Promotion</h4>
+                </div>
+            </div>
+
+            <Row>
+                <UpdatePromotion handleUpdatePromotion={handleUpdatePromotion} promotion={promotion} />
+            </Row>
+        </>
+    );
+};
