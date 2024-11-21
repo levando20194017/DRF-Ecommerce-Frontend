e5121ae619489bb6a90aec45d72a0b7431bf890e
Do LE VAN VTI(D9) <do.levan@vti.com.vn>
2024-11-21 07:25:10 +0700

    fix home page

--- Viva_phone_client/src/components/Home/HomeBanner/index.tsx
+++ Viva_phone_client/src/components/Home/HomeBanner/index.tsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { useSpring, animated } from "@react-spring/web";
 import { GrLinkNext } from "react-icons/gr";
+import { PiMouseSimpleLight } from "react-icons/pi";
+import { IoMdArrowDown } from "react-icons/io";
+
 import "../style.scss";
+import { FaChevronDown } from "react-icons/fa";
 const videoSource: string = require("../../../assets/video/videoBanner.mp4");
 
 export const HomeBanner = () => {
@@ -126,8 +130,11 @@ export const HomeBanner = () => {
           className="video-banner"
         />
       </div>
+      <div className="frame-icon-mouse">
+        <PiMouseSimpleLight style={{fontSize: "40px"}}/>
+        <IoMdArrowDown style={{fontSize: "20px"}}/>
+      </div>
       <div className="div-shadow">
-
       </div>
     </div>
   );
--- Viva_phone_client/src/screens/Home/index.tsx
+++ Viva_phone_client/src/screens/Home/index.tsx
@@ -2,20 +2,35 @@ import { ListBlog } from "../../components/Blog/ListBlog";
 import { HomeBanner } from "../../components/Home/HomeBanner";
 import { ProductCatgories } from "../../components/Home/ProductCatgories";
 import TopCategories from "../../components/Home/TopCategories";
+import { FaHandPointRight } from "react-icons/fa";
+import './home.scss'
+import { Link } from "react-router-dom";
+import { Routes } from "../Routes";
 export const HomePage = () => {
   return (
     <div>
       <HomeBanner />
-        <TopCategories />
-        <ProductCatgories />
-        <div className="list-blogs container mt-5">
-        <div className="title">Tin tức gần đây</div>
-          <div className="text-center description">
-              <p>
-                  Tìm hiểu thêm nhiều thông tin về các sản phẩm mới nhất được cập nhật dưới đây
-              </p>
+      <TopCategories />
+      <ProductCatgories />
+      <div className="list-blogs container mt-5">
+            <div className="title">Tin tức gần đây</div>
+            <div className="text-center description">
+                <p>
+                    Tìm hiểu thêm nhiều thông tin về các sản phẩm mới nhất được cập nhật dưới đây
+                </p>
+            </div>
+            <ListBlog />
+      </div>
+      <div className="contact-us container mt-4">
+          <div className="frame-blur"></div>
+          <div className="contact-title">
+              Liên hệ với đội ngũ của chúng tôi
           </div>
-          <ListBlog />
+          <Link to={Routes.Contact.path}>
+            <div className="icon-arrow-right">
+              <FaHandPointRight style={{fontSize:"64px"}}/>
+            </div>
+          </Link>
         </div>
     </div>
   );
